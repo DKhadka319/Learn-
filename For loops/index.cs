ef2d01c4cd915eac5c56d112a96fd87a8ea8a37b
Dhiraj Khadka <89305722+DKhadka319@users.noreply.github.com>
2022-01-28 08:16:54 +1100

    Create index.cs

---
+++ For loops/index.cs
@@ -0,0 +1,16 @@
+using System;
+namespace CSharp
+{
+    class Program
+    {
+        static void Main(string[]args)
+        {
+            for(int counter = 0; counter < 10; counter++)
+{
+  Console.WriteLine($"Hello World! The counter is {counter}");
+}
+
+            }
+        
+        }
+    }
