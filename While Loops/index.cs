0dcafe7623ad00ce9969580202a17ecb61bd8d6a
Dhiraj Khadka <89305722+DKhadka319@users.noreply.github.com>
2022-01-28 08:14:10 +1100

    Create index.cs

---
+++ While Loops/index.cs
@@ -0,0 +1,16 @@
+using System;
+namespace CSharp
+{
+    class Program
+    {
+        static void Main(string[]args)
+        {
+            int counter = 0;
+            while (counter < 1)
+            {
+                Console.WriteLine($"Hello World! The counter is {counter}");
+                counter++;
+            }
+        
+        }
+    }
