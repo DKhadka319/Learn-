44bfa801ab03bb6da15df35b553635d4bde6842b
Dhiraj Khadka <89305722+DKhadka319@users.noreply.github.com>
2022-01-28 08:15:21 +1100

    Create index.js

---
+++ Do while loops/index.js
@@ -0,0 +1,18 @@
+using System;
+namespace CSharp
+{
+    class Program
+    {
+        static void Main(string[]args)
+        {
+            int counter = 0;
+do
+{
+  Console.WriteLine($"Hello World! The counter is {counter}");
+  counter++;
+} while (counter < 10);
+
+            }
+        
+        }
+    }
